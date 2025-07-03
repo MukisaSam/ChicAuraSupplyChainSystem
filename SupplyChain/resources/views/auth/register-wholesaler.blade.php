2ad09c231fbb52df58c0f78c5621815bafcd7e50
MusiMark <82577689+MusiMark@users.noreply.github.com>
2025-07-04 00:39:37 +0300

    small edits

--- SupplyChain/resources/views/auth/register-wholesaler.blade.php
+++ SupplyChain/resources/views/auth/register-wholesaler.blade.php
@@ -162,12 +162,13 @@
                         </div>
                     </div>
 
+                    <!--
                     <div>
                         <label for="monthly_order_volume" class="form-label block text-sm font-medium">Expected Monthly Order Volume (units)</label>
                         <div class="mt-1">
                             <input id="monthly_order_volume" name="monthly_order_volume" type="number" required class="form-input appearance-none block w-full px-3 py-2 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                         </div>
-                    </div>
+                    </div>-->
 
                     <div>
                         <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500">
